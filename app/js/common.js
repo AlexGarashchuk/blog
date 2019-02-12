3ab827e3fb7faa9b223ecf49f520049bdb377684
Alex <algaraschukicloud.com@Alex.Speedport_W723_V_Typ_A_1_01_018>
2019-02-13 00:29:17 +0100

    del ignore files

--- app/js/common.js
+++
@@ -1,7 +0,0 @@
-$('.burger').click(function(){
-    $('.hidden-menu').slideToggle(500),
-        $('.burger').toggleClass('active'),
-        $('.center-burger').toggleClass('center-burger_hidden');
-})
-
-
